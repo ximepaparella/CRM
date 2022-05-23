596a54f0aeebdd3d47eb09c43325f0c1e95ec701
ximena.paparella@karvi.com <ximenapaparella@MacBook-Pro-de-Ximena.local>
2022-05-23 09:33:10 -0300

    Se instalan dependencias de mongoose y dotenv

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,32 +1,7 @@
-// armo datos Mock para consultar con el resolver
-const cursos = [
-  {
-    titulo: "Javacript Moderno Guía definitiva construye +10 proyectos",
-    tecnologia: "Javacript ES6",
-  },
-  {
-    titulo: "React- La guia completa: Hooks Context Redux MERN +15 Apps",
-    tecnologia: "React",
-  },
-  {
-    titulo: "Node.js - Bootcamp desarrollo web inc. MVC y REST APIs",
-    tecnologia: "Node.js",
-  },
-  {
-    titulo: "React JS Avazado - Fullstack React GraphQL y Apollo",
-    tecnologia: "React",
-  },
-];
-
 //Resolvers
 const resolvers = {
   Query: {
-    obtenerCursos: (_, { input }, ctx) => {
-      const resultado = cursos.filter(
-        (curso) => curso.tecnologia === input.tecnologia
-      );
-      return resultado;
-    },
+    obtenerCurso: () => "Algo",
   },
 };
 
--- db/schema.js
+++ db/schema.js
@@ -2,21 +2,8 @@ const { gql } = require("apollo-server");
 
 //Schema
 const typeDefs = gql`
-  type Curso {
-    titulo: String
-  }
-
-  type Tecnologia {
-    tecnologia: String
-  }
-
-  input CursoInput {
-    tecnologia: String
-  }
-
   type Query {
-    obtenerCursos(input: CursoInput!): [Curso]
-    obtenerTecnologia: [Tecnologia]
+    obtenerCurso: String
   }
 `;
 
